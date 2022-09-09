3f6c58e7f13c58d079ad3baf09205d1d6c357e22
Tuhin Mridha <tuhinmridha11@gmail.com>
2022-09-10 03:50:36 +0600

    Scriptable Singleton Script Folder and Namespace Restructured

--- Assets/_Scripts/ScriptableSingletonSystem/GameControlManager.cs
+++ Assets/_Scripts/ScriptableSingletonSystem/GameControlManager.cs
@@ -1,17 +1,26 @@
 using UnityEngine;
 
-namespace _Scripts.Singleton_System
+namespace _Scripts.ScriptableSingletonSystem
 {
     [CreateAssetMenu(menuName = "Custom/GameControlManager")]
     public class GameControlManager : SingletonScriptableObject<GameControlManager>
     {
         [SerializeField] private VariablesSingleton _variablesSingleton;
+        [SerializeField] private PreferenceSingleton _preferenceSingleton;
 
         public static VariablesSingleton VariablesSingleton
         {
             get
             {
                 return Instance._variablesSingleton;
+            }
+        }
+
+        public static PreferenceSingleton PreferenceSingleton
+        {
+            get
+            {
+                return Instance._preferenceSingleton;
             }
         }
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
--- Assets/_Scripts/ScriptableSingletonSystem/VariablesSingleton.cs
+++ Assets/_Scripts/ScriptableSingletonSystem/VariablesSingleton.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-namespace _Scripts.Singleton_System
+namespace _Scripts.ScriptableSingletonSystem
 {
     [CreateAssetMenu(menuName = "Custom/VariablesSingleton")]
     public class VariablesSingleton : ScriptableObject
